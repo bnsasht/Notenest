4d6066d2944691928f78d29694530a43970138a3
bnsasht <binishasht.dev@gmail.com>
2026-02-20 15:01:48 +0545

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,8 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material3:material3:1.3.1")
+
 
 
     implementation(libs.androidx.credentials)
